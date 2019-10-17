679e302fcb03cf609dd13db5607dad6c3042d599
ajblocker <ajblocke@gmail.com>
2019-10-17 16:47:27 -0400

    major changes

--- routes/users.js
+++ routes/users.js
@@ -16,14 +16,23 @@ let User = require('../models/user.model');
 // 1. get all users on record
 // GET: /
 // ========================================
-router.get("/", async (req, res) => {
+// router.use('*', (req, res) => {
+//     // console.log(req.url, req.method)
+//     // console.log(req.body)
+//     // console.log(req.originalUrl)
+//     // console.log(req.baseUrl)
+// })
+router.get("/", (req, res) => {
+    console.log(req.url, "hello world")
     try {
-        User.findMany({})
+        User.find({})
         .then(result => {
+            console.log(result)
             res.json(result)
         })
     }
     catch(err) {
+        console.log(err)
         res.json(err);
     }
 }),
@@ -33,6 +42,7 @@ router.get("/", async (req, res) => {
 // POST /
 // ========================================
 router.post("/add", (req, res) => {
+    console.log(req.body)
     try {
         User.create(req.body)
         .then(dbUser => {
